7728f1d18415a41467056a1de0e4c0a8e5bb4577
nirajsanghvi <niraj.sanghvi@gmail.com>
2016-06-15 17:35:30 -0700

    Add option for a tooltip

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,4 +1,6 @@
 chrome.extension.sendMessage({}, function(response) {
+	var showTooltip = false;
+
 	var readyStateCheckInterval = setInterval(function() {
 	if (document.readyState === "complete") {
 		clearInterval(readyStateCheckInterval);
@@ -16,22 +18,37 @@ chrome.extension.sendMessage({}, function(response) {
 		}
 
 		jQuery.each(jQuery('h3 > a, .m-rock-trending-discussions .title a, .m-new-articles__story a, .m-rock-read-this a'), function(){
+			if (showTooltip) {
+				jQuery(this).prop('title', jQuery(this).text());
+			}
 			jQuery(this).text(link_to_title(jQuery(this).prop('href')));		
 		});
 
 		jQuery.each(jQuery('.m-hero__slot-link'), function(){
+			if (showTooltip) {
+				jQuery(this).prop('title', jQuery(this).find('h2').text());
+			}
 			jQuery(this).find('h2').text(link_to_title(jQuery(this).prop('href')));
 		});
 		
 		jQuery.each(jQuery('a.m-carousel__item, .m-big-stories__story a, .m-big-stories__main > a, .m-video-hero__main-entry > a, .p-rock__basic-article-list a'), function(){
+			if (showTooltip) {
+				jQuery(this).prop('title', jQuery(this).find('h3').text());
+			}
 			jQuery(this).find('h3').text(link_to_title(jQuery(this).prop('href')));
 		});
 		
 		jQuery.each(jQuery('.m-rock-recent-reviews__entry-link, .m-rock-popular-features__entry, .m-video-hero__playlist-entry > a'), function(){
+			if (showTooltip) {
+				jQuery(this).prop('title', jQuery(this).find('h4').text());
+			}
 			jQuery(this).find('h4').text(link_to_title(jQuery(this).prop('href')));
 		});
 		
 		jQuery.each(jQuery('.p-entry-nav a'), function(){
+			if (showTooltip) {
+				jQuery(this).prop('title', jQuery(this).find('span').text());
+			}
 			jQuery(this).find('span').text(link_to_title(jQuery(this).prop('href')));
 		});		
 
